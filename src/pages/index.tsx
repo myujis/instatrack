177ee09bb87ea5fcd426ae427dc988e9fbe6a805
Matheus Yuji <matheus.y.shiraishi@gmail.com>
2021-02-12 12:10:46 -0300

    feat: initial project structure

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import Head from 'next/head';
+
+export default function Home(): JSX.Element {
+    return (
+        <div className="content-wrapper">
+            <h1>Hello World</h1>
+        </div>
+    );
+}
