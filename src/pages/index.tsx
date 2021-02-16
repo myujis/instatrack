15e61917205350dd02d254d9f9133a7f18dce617
Matheus Yuji <matheus.y.shiraishi@gmail.com>
2021-02-16 16:40:11 -0300

    feat: initial page in progress

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,24 @@
-import Head from 'next/head';
+import styles from '../styles/index.module.css';
 
 export default function Home(): JSX.Element {
     return (
-        <div className="content-wrapper">
-            <h1>Hello World</h1>
+        <div className={styles.content_wrapper}>
+            <header className={styles.header}>
+                <h1>InstaTrack</h1>
+            </header>
+            <div className={styles.card}>
+                <div className={styles.profile_picture} />
+                <div className={styles.info}>
+                    <div className={styles.info_posts}>
+                        <p>Posts</p>
+                        <p>193</p>
+                    </div>
+                    <div className={styles.info_following}>
+                        <p>Following</p>
+                        <p>2938</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
